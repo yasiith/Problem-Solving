11e60ec02dae2430d64307547f591098dbb08f36
yasiith <yasithvimukthi259@gmail.com>
2025-03-17 23:42:47 +0530

    explanation comments are added

--- Hackerrank-solutions/Data Structures/2D Arrays-DS/solution.py
+++ Hackerrank-solutions/Data Structures/2D Arrays-DS/solution.py
@@ -1,10 +1,16 @@
 # 6 x 6 2D array, maximum hourglass sum
 
 def hourglassSum(arr):
+    # max_sum is defined as a minimum value
     max_sum = float('-inf')
+    # Loop through the 2D array
+    # looping occurs from 1 to 4 centered values of the hourglass to prevent out of bound indices
+    # looping occurs from row 1 to row 4 and column 1 to column 4
     for i in range(1,5):
         for j in range(1,5):
+            # calculate the sum of the hourglass
             hg_sum = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1]
+            # update the max_sum if the current hourglass sum is greater
             max_sum = max(max_sum, hg_sum)
 
     return max_sum
